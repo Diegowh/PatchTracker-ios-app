f83ef0f1e3a0ba6c9963d67a711ca9e68aeca235
Diegowh <diegogarcia2312@gmail.com>
2023-06-04 22:41:21 +0200

    Initial Commit

---
+++ PatchTrackerApp/PatchTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatchTrackerAppApp.swift
+//  PatchTrackerApp
+//
+//  Created by Diego García Pérez on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PatchTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
