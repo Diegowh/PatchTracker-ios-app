4931dbf3cf453127236e15c4fc44bfe3a762d881
Diegowh <diegogarcia2312@gmail.com>
2023-06-07 14:11:26 +0200

    Add cache usage to API Requests

--- PatchTrackerApp/Data/APIService.swift
+++ PatchTrackerApp/Data/APIService.swift
@@ -14,17 +14,28 @@ let localhostContentsURL = "http://127.0.0.1:8000/ValorantPatchTracker/contents/
 class APIService {
     static let shared = APIService()
     
+    init() {
+        let memoryCapacity = 500 * 1024 * 1024 // 500 MB
+        let diskCapacity = 500 * 1024 * 1024 // 500 MB
+        let urlCache = URLCache(memoryCapacity: memoryCapacity, diskCapacity: diskCapacity, diskPath: nil)
+        URLCache.shared = urlCache
+    }
+    
     func fetchEpisodes(completion: @escaping ([Episode]) -> Void) {
+        printCacheUsage()
+        
         guard let url = URL(string: localhostEpisodesURL) else {
             return
         }
         
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let data = data {
                 do {
                     let episodes = try JSONDecoder().decode([Episode].self, from: data)
                     DispatchQueue.main.async {
                         completion(episodes)
+                        printCacheUsage()
                     }
                 } catch let error {
                     print(error)
@@ -37,8 +48,8 @@ class APIService {
         guard let url = URL(string: localhostPatchNotesURL) else {
             return
         }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let data = data {
                 do {
                     let patchNotes = try JSONDecoder().decode([PatchNote].self, from: data)
@@ -53,19 +64,21 @@ class APIService {
     }
     
     func fetchContent(for patchNote: PatchNote, completion: @escaping (Content) -> Void) {
+        printCacheUsage()
         let url = URL(string: "\(localhostContentsURL)?patch_note=\(patchNote.id)")
         
         guard let unwrappedUrl = url else {
             return
         }
-        
-        URLSession.shared.dataTask(with: unwrappedUrl) { (data, response, error) in
+        let request = URLRequest(url: unwrappedUrl, cachePolicy: .returnCacheDataElseLoad)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let data = data {
                 do {
                     let contents = try JSONDecoder().decode([Content].self, from: data)
                     if let content = contents.first {
                         DispatchQueue.main.async {
                             completion(content)
+                            printCacheUsage()
                         }
                     }
                 } catch let error {
@@ -74,6 +87,5 @@ class APIService {
             }
         }.resume()
     }
-
 }
 
