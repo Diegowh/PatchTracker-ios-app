89e8c02262f33b100fece71bd66e6da49e530fab
Diegowh <diegogarcia2312@gmail.com>
2023-06-25 18:40:53 +0200

    Modify main view.

--- PatchTrackerApp/PatchTrackerAppApp.swift
+++ PatchTrackerApp/PatchTrackerAppApp.swift
@@ -9,21 +9,24 @@ import SwiftUI
 
 @main
 struct PatchTrackerAppApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            
             TabView {
                 MainView()
                     .tabItem() {
-                        Image(systemName: "house.circle")
+                        Image(systemName: "house.fill")
+                        Text("Home")
                     }
                 EpisodeListView()
-                    .tabItem {
-                        Image(systemName: "v.circle")
+                    .tabItem() {
+                        Image(systemName: "cross.circle.fill")
+                        Text("Valorant")
                     }
                 SeasonListView()
-                    .tabItem {
-                        Image(systemName: "l.circle")
+                    .tabItem() {
+                        Image(systemName: "l.square")
+                        Text("LoL")
                     }
             }
         }
